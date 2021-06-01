aed7fbf3dc27379308d4f1ba1369d77261ab7759
HIROKO1891 <85172487+HIROKO1891@users.noreply.github.com>
2021-06-02 01:18:55 +0900

    Update comment.js

--- comment.js
+++ comment.js
@@ -1,60 +1,54 @@
 <!-- customize labels of htmlcommentbox.com -->
 <script>
-/* This code goes ABOVE the main HTML Comment Box code!
- replace the text in the single quotes below to customize labels.*/
+// This code goes ABOVE the main HTML Comment Box code!
+// replace the text in the single quotes below to customize labels.
 hcb_user = {
-    /* L10N */
-    comments_header : 'Comments',
-    name_label : 'Name',
-    content_label: 'Enter your comment here',
-    submit : 'Comment',
-    logout_link : '<img title="log out" src="https://www.htmlcommentbox.com/static/images/door_out.svg" alt="[logout]" class="hcb-icon hcb-door-out"/>',
-    admin_link : '<img src="https://www.htmlcommentbox.com/static/images/door_in.svg" alt="[login]" class="hcb-icon hcb-door-in"/>',
-    no_comments_msg: 'No one has commented yet. Be the first!',
-    add:'Add your comment',
-    again: 'Post another comment',
-    rss:'<img src="https://www.htmlcommentbox.com/static/images/feed.svg" class="hcb-icon" alt="rss"/> ',
-    said:'said:',
-    prev_page:'<img src="https://www.htmlcommentbox.com/static/images/arrow_left.png" class="hcb-icon" title="previous page" alt="[prev]"/>',
-    next_page:'<img src="https://www.htmlcommentbox.com/static/images/arrow_right.png" class="hcb-icon" title="next page" alt="[next]"/>',
+    comments_header : 'コメント',
+    name_label : 'お名前',
+    content_label: 'ここにコメントを入力',
+    submit : 'コメントする',
+    logout_link : '',
+    admin_link : '',
+    no_comments_msg: '',
+    add:'コメントを追加する',
+    again:'さらにコメントを入力する',
+    rss:'',
+    said:'さんのコメント',
+    prev_page:'<img src="//www.htmlcommentbox.com/static/images/arrow_left.png" class="hcb-icon" title="previous page" alt="[prev]"/>',
+    next_page:'<img src="//www.htmlcommentbox.com/static/images/arrow_right.png" class="hcb-icon" title="next page" alt="[next]"/>',
     showing:'Showing',
     to:'to',
     website_label:'website (optional)',
     email_label:'email',
-    anonymous:'Anonymous',
-    mod_label:'(mod)',
-    subscribe:'Email Me Replies',
-    add_image:'Add Image',
+    anonymous:'名無しさん',
+    mod_label:'(管理人)',
+    subscribe:'email me replies',
     are_you_sure:'Do you want to flag this comment as inappropriate?',
-
-    reply:'<img src="https://www.htmlcommentbox.com/static/images/reply.png"/> reply',
-    flag:'<img src="https://www.htmlcommentbox.com/static/images/flag.png"/> flag',
-    like:'<img src="https://www.htmlcommentbox.com/static/images/like.png"/> like',
-
-    /* dates */
-    days_ago:'days ago',
-    hours_ago:'hours ago',
-    minutes_ago:'minutes ago',
-    within_the_last_minute:'within the last minute',
-
-    msg_thankyou:'Thank you for commenting!',
-    msg_approval:'NOTE: This comment is not published until approved',
-    msg_approval_required:'Thank you for commenting! Your comment will appear once approved by a moderator.',
-
+  
+    reply:'<img src="//www.htmlcommentbox.com/static/images/reply.png"/> 返信',
+    flag:'',
+    like:'<img src="//www.htmlcommentbox.com/static/images/like.png"/> いいね',
+  
+    //dates
+    days_ago:'日前',
+    hours_ago:'時間前',
+    minutes_ago:'分前',
+    within_the_last_minute:'数秒前',
+  
+    msg_thankyou:'コメントありがとうございます',
+    msg_approval:'このコメントは許可され次第反映されます',
+    msg_approval_required:'コメントありがとうございます',
+  
     err_bad_html:'Your comment contained bad html.',
     err_bad_email:'Please enter a valid email address.',
     err_too_frequent:'You must wait a few seconds between posting comments.',
     err_comment_empty:'Your comment was not posted because it was empty!',
     err_denied:'Your comment was not accepted.',
-    err_unknown:'Your comment was blocked for unknown reasons, please report this.',
-    err_spam:'Your comment was detected as spam.',
-    err_blocked:'Your comment was blocked by site policy.',
-
-    /* SETTINGS */
+  
+    //SETTINGS
     MAX_CHARS: 8192,
-    PAGE:'', /* ID of the webpage to show comments for. defaults to the webpage the user is currently visiting. */
-    ON_COMMENT: function(){}, /* Function to call after commenting. */
-    RELATIVE_DATES:true /* show dates in the form "X hours ago." etc. */
+    PAGE:'', // ID of the webpage to show comments for. defaults to the webpage the user is currently visiting.
+    RELATIVE_DATES:true // show dates in the form "X hours ago." etc.
 };
 </script>
-<!-- done customizing labels of htmlcommentbox.com -->
+<!- done customizing labels of htmlcommentbox.com -->
